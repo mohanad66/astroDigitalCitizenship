22dd2b3347706f02718cf05f39ed2db4647b48ae
mohanad66 <mohanadmahmoud33245@gmail.com>
2024-06-26 12:43:11 +0300

    update 1

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+// window.addEventListener("scroll",function(){
+//     var posY = 0-window.scrollY
+//     var header = document.querySelector("#navbar-example2");
+//     var head = document.querySelector(".display")
+//     header.classList.toggle("display",posY)
+//   })
+//   window.addEventListener("scroll",function(){
+//   var posY = 0-window.scrollY
+//     var header = document.querySelector("#dgpar");
+//     header.classList.toggle("displa",posY)
+//   });
